1aaf5e1706c6c6635c35aa9620fcf4228c29fa7f
Fabien Garcia <fab0670312047@gmail.com>
2025-04-24 17:00:09 +0200

    Use public folder approach

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: "/platformer/",
-    publicDir: "assets",
+    publicDir: "public",
     build: {
         outDir: "dist",
     }
